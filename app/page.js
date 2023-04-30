eebccb81e3ae92560cc57a99f78288b7de660102
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-30 17:35:21 -0400

    update cards

--- app/page.js
+++ app/page.js
@@ -66,8 +66,44 @@ export default function Home() {
           </p>
         </div>
         <div>
+          {/* card 1 */}
           <div className=" text-center shadow-lg p-10 rounded-xl my-10">
             <Image src={design} width={100} height={100} className=" mx-auto" />
+            <h3 className=" text-lg font-medium pt-8 pb-2">
+              Beautiful Designs
+            </h3>
+            <p className=" py-2">
+              Creating elegant designs suited for your needs following core
+              design theory.
+            </p>
+            <h4 className=" py-4 text-teal-600">Design tools I use</h4>
+            <p className=" text-gray-800 py-1">Figma</p>
+            <p className=" text-gray-800 py-1">Photoshop</p>
+            <p className=" text-gray-800 py-1">Photoshop</p>
+          </div>
+          {/* card 2 */}
+          <div className=" text-center shadow-lg p-10 rounded-xl my-10">
+            <Image
+              src={consulting}
+              width={100}
+              height={100}
+              className=" mx-auto"
+            />
+            <h3 className=" text-lg font-medium pt-8 pb-2">
+              Beautiful Designs
+            </h3>
+            <p className=" py-2">
+              Creating elegant designs suited for your needs following core
+              design theory.
+            </p>
+            <h4 className=" py-4 text-teal-600">Design tools I use</h4>
+            <p className=" text-gray-800 py-1">Figma</p>
+            <p className=" text-gray-800 py-1">Photoshop</p>
+            <p className=" text-gray-800 py-1">Photoshop</p>
+          </div>
+          {/* card 3 */}
+          <div className=" text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={code} width={100} height={100} className=" mx-auto" />
             <h3 className=" text-lg font-medium pt-8 pb-2">
               Beautiful Designs
             </h3>
