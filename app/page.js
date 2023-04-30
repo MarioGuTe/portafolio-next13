2e0b6a36bc54a6d242328827ca6636ff2ab62f5a
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-30 02:05:44 -0400

    add services section

--- app/page.js
+++ app/page.js
@@ -42,13 +42,25 @@ export default function Home() {
           <AiFillLinkedin />
           <AiFillYoutube />
         </div>
-        <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-16 overflow-hidden">
+        <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 h-60 mt-12 overflow-hidden">
           <Image src={Mario} alt="" fill style={{ objectFit: "cover" }} />
         </div>
       </section>
       <section>
         <div>
-          <h3>Services I offer</h3>
+          <h3 className=" text-2xl py-1">Services I offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            Since the beginning of my journey as a freelance designer and
+            developer, I've done remote work for
+            <span className="text-teal-500"> agencies </span>
+            consulted for <span className="text-teal-500">startups </span>
+            and collaborated with talanted people to create digital products for
+            both business and consumer use.
+          </p>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            I offer from a wide range of services, including brand design,
+            programming and teaching.
+          </p>
         </div>
       </section>
     </main>
