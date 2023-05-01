b3193851566681676dc092dc9e77377844a3b6d9
MarioGuTe <mariogutierreztello@gmail.com>
2023-05-01 00:20:38 -0400

    add skills icons

--- app/page.js
+++ app/page.js
@@ -2,10 +2,13 @@
 import { useState } from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import {
-  AiFillTwitterCircle,
-  AiFillLinkedin,
-  AiFillYoutube,
-} from "react-icons/ai";
+  DiJsBadge,
+  DiCss3Full,
+  DiHtml5,
+  DiGit,
+  DiReact,
+  DiNodejsSmall,
+} from "react-icons/di";
 import Image from "next/image";
 import Mario from "../public/mario-avatar.png";
 import design from "../public/design.png";
@@ -56,10 +59,13 @@ export default function Home() {
               desarrollador.
             </p>
           </div>
-          <div className=" text-5xl flex justify-center gap-14 py-3 text-gray-400">
-            <AiFillTwitterCircle />
-            <AiFillLinkedin />
-            <AiFillYoutube />
+          <div className=" text-5xl flex justify-center gap-14 py-3 text-violet-500">
+            <DiHtml5 />
+            <DiCss3Full />
+            <DiJsBadge />
+            {/* <DiGit /> */}
+            <DiReact />
+            <DiNodejsSmall />
           </div>
           <div className=" relative mx-auto bg-gradient-to-b from-violet-600 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={Mario} alt="" fill style={{ objectFit: "cover" }} />
