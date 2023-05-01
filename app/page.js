c8caa0445eda7480ff5df9e3dfcf4bee511f5a0b
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-30 23:40:06 -0400

    add darkMode logic

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import {
   AiFillTwitterCircle,
@@ -17,152 +19,201 @@ import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <main className=" bg-white px-10 md:px-20 lg:px-40">
-      <section className="min-h-screen">
-        <nav className=" py-10 mb-12 flex justify-between">
-          <h1 className=" text-base font-burtons">MARIO GUTIÉRREZ</h1>
-          <ul className=" flex items-center">
-            <li>
-              <BsFillMoonStarsFill className=" cursor-pointer text-xl" />
-            </li>
-            <li>
-              <a
-                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-2 py-2 rounded-md ml-4"
-                href="#"
-              >
-                Curriculum
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="text-center p-10">
-          <h2 className=" text-5xl py-2 text-teal-600 font-medium md:text-6xl">
-            Mario Gutiérrez
-          </h2>
-          <h3 className=" text-2xl py-2 md:text-3xl">
-            Desarrollador Fullstack JavaScript
-          </h3>
-          <p className=" text-lg py-5 leading-8 text-gray-800 md:text-xl">
-            El cariño por aprender es la mejor habilidad que puede cultivar un
-            desarrollador.
-          </p>
-        </div>
-        <div className=" text-5xl flex justify-center gap-14 py-3 text-gray-600">
-          <AiFillTwitterCircle />
-          <AiFillLinkedin />
-          <AiFillYoutube />
-        </div>
-        <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 h-60 mt-12 overflow-hidden">
-          <Image src={Mario} alt="" fill style={{ objectFit: "cover" }} />
-        </div>
-      </section>
-      <section>
-        <div className="mt-8">
-          <h3 className=" text-2xl py-2">Services I offer</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            Since the beginning of my journey as a freelance designer and
-            developer, I've done remote work for
-            <span className="text-teal-500"> agencies </span>
-            consulted for <span className="text-teal-500">startups </span>
-            and collaborated with talanted people to create digital products for
-            both business and consumer use.
-          </p>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            I offer from a wide range of services, including brand design,
-            programming and teaching.
-          </p>
-        </div>
-        <div>
-          {/* card 1 */}
-          <div className=" text-center shadow-lg p-10 rounded-xl my-10">
-            <Image src={design} width={100} height={100} className=" mx-auto" />
-            <h3 className=" text-lg font-medium pt-8 pb-2">
-              Beautiful Designs
+    <div className={darkMode ? "dark" : ""}>
+      <main className={` bg-white px-10 md:px-20 lg:px-40  dark:bg-gray-900`}>
+        <section className="min-h-screen">
+          <nav className=" py-10 mb-12 flex justify-between">
+            <h1 className=" text-base font-burtons">MARIO GUTIÉRREZ</h1>
+            <ul className=" flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-2 py-2 rounded-md ml-4"
+                  href="#"
+                >
+                  Curriculum
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className=" text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+              Mario Gutiérrez
+            </h2>
+            <h3 className=" text-2xl py-2 md:text-3xl">
+              Desarrollador Fullstack JavaScript
             </h3>
-            <p className=" py-2">
-              Creating elegant designs suited for your needs following core
-              design theory.
+            <p className=" text-lg py-5 leading-8 text-gray-800 md:text-xl">
+              El cariño por aprender es la mejor habilidad que puede cultivar un
+              desarrollador.
             </p>
-            <h4 className=" py-4 text-teal-600">Design tools I use</h4>
-            <p className=" text-gray-800 py-1">Figma</p>
-            <p className=" text-gray-800 py-1">Photoshop</p>
-            <p className=" text-gray-800 py-1">Photoshop</p>
           </div>
-          {/* card 2 */}
-          <div className=" text-center shadow-lg p-10 rounded-xl my-10">
-            <Image
-              src={consulting}
-              width={100}
-              height={100}
-              className=" mx-auto"
-            />
-            <h3 className=" text-lg font-medium pt-8 pb-2">
-              Beautiful Designs
-            </h3>
-            <p className=" py-2">
-              Creating elegant designs suited for your needs following core
-              design theory.
+          <div className=" text-5xl flex justify-center gap-14 py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillLinkedin />
+            <AiFillYoutube />
+          </div>
+          <div className=" relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image src={Mario} alt="" fill style={{ objectFit: "cover" }} />
+          </div>
+        </section>
+        <section>
+          <div className="mt-8">
+            <h3 className=" text-2xl py-2">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
             </p>
-            <h4 className=" py-4 text-teal-600">Design tools I use</h4>
-            <p className=" text-gray-800 py-1">Figma</p>
-            <p className=" text-gray-800 py-1">Photoshop</p>
-            <p className=" text-gray-800 py-1">Photoshop</p>
-          </div>
-          {/* card 3 */}
-          <div className=" text-center shadow-lg p-10 rounded-xl my-10">
-            <Image src={code} width={100} height={100} className=" mx-auto" />
-            <h3 className=" text-lg font-medium pt-8 pb-2">
-              Beautiful Designs
-            </h3>
-            <p className=" py-2">
-              Creating elegant designs suited for your needs following core
-              design theory.
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
             </p>
-            <h4 className=" py-4 text-teal-600">Design tools I use</h4>
-            <p className=" text-gray-800 py-1">Figma</p>
-            <p className=" text-gray-800 py-1">Photoshop</p>
-            <p className=" text-gray-800 py-1">Photoshop</p>
           </div>
-        </div>
-      </section>
-      <section>
-        <div>
-          <h3 className=" text-3xl py-1">Portafolio</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            Since the beginning of my journey as a freelance designer and
-            developer, I've done remote work for
-            <span className="text-teal-500"> agencies </span>
-            consulted for <span className="text-teal-500">startups </span>
-            and collaborated with talanted people to create digital products for
-            both business and consumer use.
-          </p>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            I offer from a wide range of services, including brand design,
-            programming and teaching.
-          </p>
-        </div>
-        <div>
+          <div className=" lg:flex gap-10">
+            {/* card 1 */}
+            <div className=" text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={design}
+                width={100}
+                height={100}
+                className=" mx-auto"
+              />
+              <h3 className=" text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className=" py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className=" py-4 text-teal-600">Design tools I use</h4>
+              <p className=" text-gray-800 py-1">Figma</p>
+              <p className=" text-gray-800 py-1">Photoshop</p>
+              <p className=" text-gray-800 py-1">Photoshop</p>
+            </div>
+            {/* card 2 */}
+            <div className=" text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={consulting}
+                width={100}
+                height={100}
+                className=" mx-auto"
+              />
+              <h3 className=" text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className=" py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className=" py-4 text-teal-600">Design tools I use</h4>
+              <p className=" text-gray-800 py-1">Figma</p>
+              <p className=" text-gray-800 py-1">Photoshop</p>
+              <p className=" text-gray-800 py-1">Photoshop</p>
+            </div>
+            {/* card 3 */}
+            <div className=" text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={code} width={100} height={100} className=" mx-auto" />
+              <h3 className=" text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className=" py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className=" py-4 text-teal-600">Design tools I use</h4>
+              <p className=" text-gray-800 py-1">Figma</p>
+              <p className=" text-gray-800 py-1">Photoshop</p>
+              <p className=" text-gray-800 py-1">Photoshop</p>
+            </div>
+          </div>
+        </section>
+        <section>
           <div>
-            <Image src={web1} />
+            <h3 className=" text-3xl py-1">Portafolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
           </div>
-          <div>
-            <Image src={web2} />
+          {/* portafolio projects section */}
+          <div className=" flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className=" basis-1/3 flex-1">
+              <Image
+                src={web1}
+                className=" rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                responsive
+              />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image
+                src={web2}
+                className=" rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                responsive
+              />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image
+                src={web3}
+                className=" rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                responsive
+              />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image
+                src={web4}
+                className=" rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                responsive
+              />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image
+                src={web5}
+                className=" rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                responsive
+              />
+            </div>
+            <div className=" basis-1/3 flex-1">
+              <Image
+                src={web6}
+                className=" rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                responsive
+              />
+            </div>
           </div>
-          <div>
-            <Image src={web3} />
-          </div>
-          <div>
-            <Image src={web4} />
-          </div>
-          <div>
-            <Image src={web5} />
-          </div>
-          <div>
-            <Image src={web6} />
-          </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </div>
   );
 }
