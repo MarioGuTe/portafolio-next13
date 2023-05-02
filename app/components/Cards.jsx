82f6227e65ff60ac6ea4bd7ff6aa376bf07d427c
MarioGuTe <mariogutierreztello@gmail.com>
2023-05-02 19:02:04 -0400

    update card info and style

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -19,9 +19,9 @@ const Cards = () => {
         </p>
       </div>
       {/* Cards Section */}
-      <div className=" lg:flex gap-10">
+      <div className=" lg:flex gap-10 px-6 md:px-20 lg:px-40">
         {/* card 1 */}
-        <div className=" text-center shadow-lg p-10 rounded-xl my-10 shadow-purple-500">
+        <div className=" text-center  border-solid border-4 border-violet-100 p-10 rounded-xl my-10 shadow-purple-500">
           <Image src={design} width={100} height={100} className=" mx-auto" />
           <h3 className=" text-2xl font-semibold pt-8 pb-2  text-violet-500">
             Desarrollo Web
@@ -30,55 +30,41 @@ const Cards = () => {
             Me gusta el proceso de tomar una idea por más pequeña o grande que
             sea y darle vida a través del desarrollo web.
           </p>
-          <h4 className=" py-4 text-xl font-normal text-violet-500">Skills</h4>
-          <div className="flex flex-wrap">
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              HTML
-            </p>
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              CSS
-            </p>
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              JavaScript
-            </p>
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              ReactJS
-            </p>
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              NodeJS
-            </p>
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              Git
-            </p>
+          <h4 className=" py-4 text-xl font-medium text-violet-500">Skills</h4>
+          <div>
+            <p className="text-gray-800 py-1  dark:text-white">HTML</p>
+            <p className="text-gray-800 py-1  dark:text-white">CSS</p>
+            <p className="text-gray-800 py-1  dark:text-white">JavaScript</p>
+            <p className="text-gray-800 py-1  dark:text-white">ReactJS</p>
+            <p className="text-gray-800 py-1  dark:text-white">NodeJS</p>
+            <p className="text-gray-800 py-1  dark:text-white">Git</p>
           </div>
         </div>
         {/* card 2 */}
-        <div className=" text-center shadow-lg p-10 rounded-xl my-10 shadow-purple-500">
+        <div className=" text-center border-solid border-4 border-violet-100 p-10 rounded-xl my-10 shadow-purple-500">
           <Image src={design} width={100} height={100} className=" mx-auto" />
           <h3 className=" text-2xl font-semibold pt-8 pb-2  text-violet-500">
-            Educación
+            Formación
           </h3>
           <p className=" py-2  dark:text-white">
-            Creating elegant designs suited for your needs following core design
-            theory.
+            Saber que hay un mundo de cosas nuevas por aprender es una de las
+            razones por las que me levanto cada mañana.
           </p>
-          <h4 className=" py-4 text-xl font-normal text-violet-500">
-            Design tools I use
+          <h4 className=" py-4 text-xl font-medium text-violet-500">
+            Certificaciones y Logros
           </h4>
-          <div className="flex flex-wrap">
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              Figma
+          <div>
+            <p className="text-gray-800 py-1  dark:text-white">
+              Desarrollador Fullstack Javascript (Bootcamp Desafío Latam 2022
+              -2023)
             </p>
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              Photoshop
-            </p>
-            <p className=" basis-1/3 flex-1 text-gray-800 py-1  dark:text-white">
-              Photoshop
+            <p className="text-gray-800 py-1  dark:text-white">
+              React JS Fundamentals (Escalaba: Tech Talents Hub 2023)
             </p>
           </div>
         </div>
         {/* card 3 */}
-        <div className=" text-center shadow-lg p-10 rounded-xl my-10 shadow-purple-500">
+        <div className=" text-center border-solid border-4 border-violet-100 p-10 rounded-xl my-10 shadow-purple-500">
           <Image src={design} width={100} height={100} className=" mx-auto" />
           <h3 className=" text-2xl font-semibold pt-8 pb-2  text-violet-500">
             Diseño
@@ -87,7 +73,7 @@ const Cards = () => {
             Creating elegant designs suited for your needs following core design
             theory.
           </p>
-          <h4 className=" py-4 text-xl font-normal text-violet-500">
+          <h4 className=" py-4 text-xl font-medium text-violet-500">
             Design tools I use
           </h4>
           <div className="flex flex-wrap">
