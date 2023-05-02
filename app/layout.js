58dbb588effa3e80690b9d28b4c57b9835b1d45b
MarioGuTe <mariogutierreztello@gmail.com>
2023-05-02 17:37:17 -0400

    add roboto mono font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Roboto_Mono({
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Mario Gutiérrez Portafolio",
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
