f7d3a8dc15896bfb085d2c840ab5774e47f16c07
MarioGuTe <mariogutierreztello@gmail.com>
2023-05-15 22:21:39 -0400

    update responsive layout

--- app/components/Landing.jsx
+++ app/components/Landing.jsx
@@ -24,18 +24,18 @@ const Landing = () => {
           desarrollador.
         </p>
       </div>
-      <div className=" text-4xl flex flex-col justify-center gap-4 py-1 px-6 text-violet-500 lg:flex-row lg:gap-0 lg:text-5xl lg:pt-0 ">
-        <div className=" flex justify-center">
+      <div className=" text-4xl flex flex-col justify-center gap-4 py-1 px-6 text-violet-500 md:flex-row md:gap-0 lg:text-5xl lg:pt-0 ">
+        <div className=" flex justify-center gap-2">
           <DiHtml5 />
           <DiCss3Full />
           <DiJsBadge />
         </div>
-        <div className=" flex justify-center">
+        <div className=" flex justify-center gap-2 ml-2">
           <DiReact />
           <DiNodejsSmall />
         </div>
       </div>
-      <div className=" relative mx-auto bg-gradient-to-b from-violet-600 rounded-full w-44 h-44 mt-12 overflow-hidden md:h-72 md:w-72 lg:mt-4 lg:h-52 lg:w-52">
+      <div className=" relative mx-auto bg-gradient-to-b from-violet-600 rounded-full w-44 h-44 mt-12 overflow-hidden md:h-72 md:w-72 lg:mt-4 lg:h-52 lg:w-52 xl:mt-16">
         <Image src={Mario} alt="avatar" fill style={{ objectFit: "cover" }} />
       </div>
     </section>
