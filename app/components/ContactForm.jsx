036c6ece2f3f0a5db44432e9e3500e0ffc9e999a
MarioGuTe <mariogutierreztello@gmail.com>
2023-05-16 23:57:42 -0400

    add basic structure

--- app/components/ContactForm.jsx
+++ app/components/ContactForm.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const ContactForm = () => {
-  return <div>hola</div>;
+  return (
+    <section className=" flex justify-center">
+      <form action="" className=" border-solid border-4 border-violet-100">
+        <div>
+          <label htmlFor="name">Nombre</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="name">Correo Electrónico</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="name">Mensaje</label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+        <button>Enviar</button>
+      </form>
+    </section>
+  );
 };
 
 export default ContactForm;
