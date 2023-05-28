6cc64e9d7c7f74ac404ae5320f6505769debf3c5
MarioGuTe <mariogutierreztello@gmail.com>
2023-05-27 23:20:32 -0400

    update portfolio images

--- app/components/Portafolio.jsx
+++ app/components/Portafolio.jsx
@@ -4,8 +4,6 @@ import web1 from "/public/web1.png";
 import web2 from "/public/web2.png";
 import web3 from "/public/web3.png";
 import web4 from "/public/web4.png";
-import web5 from "/public/web5.png";
-import web6 from "/public/web6.png";
 
 const Portafolio = () => {
   return (
@@ -55,26 +53,6 @@ const Portafolio = () => {
         <div className=" basis-1/3 flex-1">
           <Image
             src={web4}
-            className=" rounded-lg object-cover"
-            width={"100%"}
-            height={"100%"}
-            responsive="true"
-            alt="photo"
-          />
-        </div>
-        <div className=" basis-1/3 flex-1">
-          <Image
-            src={web5}
-            className=" rounded-lg object-cover"
-            width={"100%"}
-            height={"100%"}
-            responsive="true"
-            alt="photo"
-          />
-        </div>
-        <div className=" basis-1/3 flex-1">
-          <Image
-            src={web6}
             className=" rounded-lg object-cover"
             width={"100%"}
             height={"100%"}
